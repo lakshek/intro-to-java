98aa19cb465daf7f22d93f5ca125f032bbcdd3f4
lakshek <email2shek@gmail.com>
2025-08-03 13:09:58 -0500

    Second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        System.out.println("Installed JDK");
+        System.out.println("Installed IntelliJ");
+        System.out.println("Created a project in IntelliJ");
+        System.out.println("Connected the project to GitHub repository");
+        System.out.println("Committed and pushed it to the repository");
+        System.out.println("Goodbye, world!");
     }
 }
